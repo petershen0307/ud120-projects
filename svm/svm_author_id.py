a91f5001065ffa1523a27b977f1e39215d52e45b
PeterShen <petershen0307@hotmail.com>
2017-11-12 18:05:15 +0800

    lesson 3 quiz 28, 29

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -25,6 +25,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+
+clf = SVC(kernel='linear')
+train_start_time = time()
+clf.fit(features_train, labels_train)
+train_end_time = time()
+print("time elapsed by training:", round(train_end_time - train_start_time, 3), "(s)")
+train_start_time = time()
+print(clf.predict(features_test))
+train_end_time = time()
+print("time elapsed by predict:", round(train_end_time - train_start_time, 3), "(s)")
+print('predict score:', clf.score(features_test, labels_test))
 #########################################################
 
 
