45f2b6c388666fb18da898f9e9b3171124329fd5
PeterShen <petershen0307@hotmail.com>
2017-11-22 23:23:04 +0800

    lesson quiz 40

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,11 +25,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 print("number of features", len(features_train[0]))
-#from sklearn import tree
-#clf = tree.DecisionTreeClassifier(min_samples_split=40)
-#clf.fit(features_train, labels_train)
-#accuracy = clf.score(features_test, labels_test)
-#print("accuracy {}".format(accuracy))
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+accuracy = clf.score(features_test, labels_test)
+print("accuracy {}".format(accuracy))
 
 #########################################################
 
