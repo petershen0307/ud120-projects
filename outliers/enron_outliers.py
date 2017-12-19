3429a72243865bd08efa0f69d2c5fdc93931d912
PeterShen <petershen0307@hotmail.com>
2017-12-20 00:00:49 +0800

    lesson 8 quiz 15 16

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -12,8 +12,17 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb"
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
 ### your code below
+def drawScatterPlot(data):
+    for point in data:
+        salary = point[0]
+        bonus = point[1]
+        matplotlib.pyplot.scatter( salary, bonus )
+    matplotlib.pyplot.xlabel("salary")
+    matplotlib.pyplot.ylabel("bonus")
+    matplotlib.pyplot.show()
 
-
-
+for k, v in data_dict.items():
+    if (v['salary'] == 26704229):
+        print(k, v['salary'])
+drawScatterPlot(data)
