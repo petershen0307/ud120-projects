affd91329a04000d16a11854ee03b8b48b57c68c
PeterShen <petershen0307@hotmail.com>
2018-01-17 23:32:51 +0800

    lesson 11 quiz 19

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -48,23 +48,29 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            extractText = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            for w in ["sara", "shackleton", "chris", "germani"]:
+                extractText = extractText.replace(w, "")
+            extractText = extractText.replace("\n", " ")
             ### append the text to word_data
+            word_data.append(extractText)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
 print("emails processed")
 from_sara.close()
 from_chris.close()
-
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+print("quiz 19, word_data[152]:",word_data[152])
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
 
 
