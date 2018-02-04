872b31275a22a3f041c41313ec0c5cd6f8cec7b6
PeterShen <petershen0307@hotmail.com>
2018-02-04 20:19:18 +0800

    lesson 14 quiz 18

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -22,14 +22,16 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb")
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys='../tools/python2_lesson13_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
 
 
 ### it's all yours from here forward!
+from sklearn.model_selection import train_test_split
+
+f_train, f_test, l_train, l_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier()
-clf.fit(features, labels)
-print(clf.score(features, labels))
-
+clf.fit(f_train, l_train)
+print(clf.score(f_test, l_test))
