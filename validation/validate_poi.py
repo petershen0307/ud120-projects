b14a49ead782540a723232a564d494b8d8dfd731
PeterShen <petershen0307@hotmail.com>
2018-02-04 20:10:05 +0800

    lesson 14 quiz 17

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,7 +15,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -27,6 +27,9 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### it's all yours from here forward!  
-
+### it's all yours from here forward!
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
+print(clf.score(features, labels))
 
